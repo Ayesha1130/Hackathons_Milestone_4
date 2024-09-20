da274468a86d260c18e4e4d5486dd644d0283927
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-09-20 22:46:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const resumeButton = document.getElementById('resumeButton');
+    const resumeEditButton = document.getElementById('edit-button');
+    const editResume = document.getElementById('Edit-resume');
+    const editResumeContent = document.getElementById('resumeContent');
+    resumeButton.addEventListener('click', () => {
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const education = document.getElementById('education').value;
+        const experience = document.getElementById('experience').value;
+        const skills = document.getElementById('skills').value;
+        editResumeContent.innerHTML = `
+    <div>
+     <form>
+     <fieldset>
+     <legend>Edit Resume</legend>
+     <label for="name">Name:</label>
+     <input type="text" id="name" value="${name}">
+     <label for="email">Email:</label>
+     <input type="text" id="email" value="${email}">
+     <label for="phone">Phone:</label>
+     <input type="text" id="phone" value="${phone}">
+     <label for="education">Education:</label>
+     <input type="text" id="education" value="${education}">
+     <label for="experience">Experience:</label>
+     <input type="text" id="experience" value="${experience}">
+     <label for="skills">Skills:</label>
+     <input type="text" id="skills" value="${skills}">
+     </fieldset>
+
+     </form>
+     </div>
+   <h2>Name</h2>
+    <p>${name}</p>
+    <h2>Email</h2>
+    <p>${email}</p>
+    <h2>Phone</h2>
+    <p>${phone}</p>
+    <h2>Education</h2>
+    <p>${education}</p>
+    <h2>Experience</h2>
+    <p>${experience}</p>
+    <h2>Skills</h2>
+    <p>${skills}</p>
+
+    `;
+        editResume.classList.remove('hidden');
+        resumeButton.classList.remove('hidden');
+    });
+    resumeEditButton.addEventListener('click', () => {
+        editResumeContent.contentEditable = 'true';
+        resumeEditButton.textContent = 'Save Resume';
+        resumeEditButton.removeEventListener('click', () => {
+            editResumeContent.contentEditable = 'false';
+            resumeEditButton.textContent = 'Edit Resume';
+        });
+    });
+});
---
+++ script.ts
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+const resumeButton = document.getElementById('resumeButton') as HTMLButtonElement;
+const resumeEditButton = document.getElementById('edit-button') as HTMLButtonElement;
+const editResume = document.getElementById('Edit-resume') as HTMLDivElement;
+const editResumeContent = document.getElementById('resumeContent') as HTMLDivElement;
+
+resumeButton.addEventListener('click',()=>{
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLInputElement).value;
+    const experience = (document.getElementById('experience') as HTMLInputElement).value;
+    const skills = (document.getElementById('skills') as HTMLInputElement).value;
+
+
+    editResumeContent.innerHTML =`
+    <div>
+     <form>
+     <fieldset>
+     <legend>Edit Resume</legend>
+     <label for="name">Name:</label>
+     <input type="text" id="name" value="${name}">
+     <label for="email">Email:</label>
+     <input type="text" id="email" value="${email}">
+     <label for="phone">Phone:</label>
+     <input type="text" id="phone" value="${phone}">
+     <label for="education">Education:</label>
+     <input type="text" id="education" value="${education}">
+     <label for="experience">Experience:</label>
+     <input type="text" id="experience" value="${experience}">
+     <label for="skills">Skills:</label>
+     <input type="text" id="skills" value="${skills}">
+     </fieldset>
+
+     </form>
+     </div>
+    `
+    editResume.classList.remove('hidden');
+    resumeButton.classList.remove('hidden');
+})
+
+resumeEditButton.addEventListener('click',()=>{
+    editResumeContent.contentEditable = 'true';
+    resumeEditButton.textContent = 'Save Resume';
+    resumeEditButton.removeEventListener('click', ()=>{
+        editResumeContent.contentEditable = 'false';
+        resumeEditButton.textContent = 'Edit Resume';
+    },)
+
+    });
+
+})
+
+
+
+
